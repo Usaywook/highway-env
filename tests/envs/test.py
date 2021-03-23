3c1b47ffe12ec1dc4f065d69ba72fbba354c3646
usaywook <tpdnr1360@snu.ac.kr>
2021-03-23 20:55:57 +0900

    test added

--- tests/envs/test.py
+++ tests/envs/test.py
@@ -2,9 +2,12 @@ import gym
 import highway_env
 
 env = gym.make("highway-v0")
-print(env.action_space)
+# env = gym.make("intersection-v0")
+
 print(env.observation_space)
+print(env.action_space)
 done = False
+
 while not done:
     action = env.action_space.sample() # Your agent code here
     obs, reward, done, info = env.step(action)
